a2106114537b4058c3590e52a8615bc6044007f4
pravin <pravinjadhav771@gmail.com>
2020-04-09 12:00:22 +0530

    receipt updated

--- src/components/Receipt.tsx
+++ src/components/Receipt.tsx
@@ -26,9 +26,10 @@ const useStyles  =  makeStyles((theme: Theme)  =>
       width: 100,
     },
     large: {
-      width: '100%',
-      height: '100%',
-      borderRadius: '15px'
+      width: '100px',
+      height: '100px',
+      borderRadius: '100%',
+      margin:'0 auto'
     },
     control: {
       padding: theme.spacing(2),
@@ -50,88 +51,23 @@ export default function ReceiptDetails() {
             <CardContent className='pt-0' >  
               <Grid spacing={2} style={style.AlignMent} >
                  <Grid item xs={12} sm={8} md={6} lg={4}>        
-                 <Card>
-                  <CardContent className='card-wrap'>
-                    
-                     <Box className='receipt-icon-b-border' pt={1} pb={3}>
-                    <Grid container spacing = {3} >
-                      <Grid item xs = {4} sm={3} md={3}>
-                        <Link to = '/practice'  target =  '_blank'>
-                          <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/>	
-                        </Link>
-                      </Grid>
-                      <Grid item xs = {8} sm={9} md={9}> 
-                        <Box fontWeight='bold'>
-                          Lions Club Mitra Mandal
-                        </Box>
-                        <Typography variant='caption'>
-                          Dattawadi , pune - 30
-                        </Typography>
-  
-                        <Typography variant='caption' className='d-block'>
-                          <span className='label-color'> Reg. no:</span> 123456
-                        </Typography>
-                        
-                        <Box mt={1} className='paid'>
-                          <Button variant="outlined" href='/events' size='small' target='_bank' className={classes.button}>Paid</Button>
-                        </Box>
-                      </Grid>
-                    </Grid>
-                  </Box>
+                 
+                <Card elevation={0} className='p-relative'>
+                  <div className="circle-shape circle-4"><span>Paid</span></div>
 
-                      <Box className='receipt-icon-b-border'> 
-                        <Grid container spacing = {3}>
-                          <Grid item xs = {6}>
-                            <Box mt={2} mb={2}> 
-                              <span className='label d-block
-                              '> Receipt Number: </span>
-                              <span className='f-bold'> #002 </span>
-                            </Box>
-                            
-                            <Box mt={2} mb={2} > 
-                              <span className='label d-block
-                              '> Full Name: </span>
-                              <span className='f-bold'> Baldev Pardesi </span>
-                            </Box>
-                            
-                            <Box mt={2} mb={2} > 
-                              <span className='label d-block
-                              '> Received by: </span>
-                              <span className='f-bold'> Akshay </span>
-                            </Box>
-                          </Grid>
-                          <Grid item xs = {6}>
-                            <Box mt={2} mb={2}> 
-                              <span className='label d-block
-                              '> Amount: </span>
-                              <span className='f-bold receipt-amout'> 5000 /- </span>
-                            </Box>
-                            <Box mt={2} mb={2} > 
-                              <span className='label d-block
-                              '> Date: </span>
-                              <span className='f-bold'> 8 December 2020 </span>
-                            </Box>
-                          </Grid>
-                        </Grid>
-                      </Box>
-                      <Box pt={3}>
-                    <Typography variant="h4" className='f-bold t-center'>
-                      Thank You!
-                    </Typography>
-                  </Box>
-                  </CardContent>
-                </Card>
-                <Card elevation={0} className='mt-30'>
-                <div className='circle'>
+                  <div className='circle'>
                       <FiberManualRecordIcon className='icon left-icon'/>
                       <FiberManualRecordIcon className='icon right-icon'/>
                     </div>
                   
                   <CardContent className='card-wrap'>
                     <Box className=' payment-suc' pt={1} pb={3}>
-                        <CheckCircleIcon className='successfully-icon'/>
-                        <h2 className="successfully-text">Payment Successfully</h2>
-                        <p className='transaction-n'>Transaction Number: 14002505500</p>
+                    <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/>	
+
+                        <h2 className="successfully-text"> Lions Club Mitra Mandal</h2>
+                        <p className='transaction-n'> Dattawadi , pune - 30</p>
+                        <p className='transaction-n'> Reg. no : 553 </p>
+
                       </Box>
                       <Box className=' py-20'> 
                         <Grid container>
@@ -168,11 +104,11 @@ export default function ReceiptDetails() {
                           </Grid>
                         </Grid>
                       </Box>
-                      {/* <Box pt={2}>
-                    <Typography variant="h5" className='f-bold t-center'>
+                      <Box pt={2}>
+                    <Typography variant="h5" className='f-bold t-center thank-you-title'>
                       Thank You!
                     </Typography>
-                  </Box> */}
+                  </Box>
                   </CardContent>
                 </Card>
               </Grid>
