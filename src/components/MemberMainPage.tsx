5adedbd97b897e6de46333f2f44858e20a1f05b4
pravin <pravinjadhav771@gmail.com>
2020-04-10 21:10:12 +0530

    member main page changes

--- src/components/MemberMainPage.tsx
+++ src/components/MemberMainPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, createStyles, fade } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import SearchIcon from '@material-ui/icons/Search'
-import { Box, WithStyles, withStyles, IconButton, Theme, TextField } from '@material-ui/core'
+import { Box, WithStyles, withStyles, IconButton, Theme, TextField, InputBase } from '@material-ui/core'
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 import DeleteIcon from '@material-ui/icons/Delete'
@@ -17,9 +17,11 @@ import { styles } from '@material-ui/pickers/views/Calendar/Calendar'
 import CloseIcon from '@material-ui/icons/Close'
 import Grid, { GridSpacing } from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) =>
+createStyles({
   root: {
-    minWidth: 275,
+	minWidth: 275,
+	padding:' 10px'
   },
   bullet: {
     display: 'inline-block',
@@ -35,10 +37,51 @@ const useStyles = makeStyles({
   button: {
 	color: 'white',
     padding: '4px 22px',
-    borderRadius: '15px',
-	backgroundColor: '#df3088'
-  }
-});
+    // borderRadius: '15px',
+	backgroundColor: '#df3088',
+  },
+  searchIcon: {
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+  },
+  search: {
+	position: 'relative',
+	marginRight:60,
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: fade(theme.palette.common.white, 0.15),
+	'&:hover': {
+	  backgroundColor: fade(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	// width: '100%',
+	[theme.breakpoints.up('sm')]: {
+	  marginLeft: theme.spacing(1),
+	  width: 'auto',
+	},
+  },
+  inputRoot: {
+	color: 'inherit',
+  },
+  inputInput: {
+	padding: theme.spacing(1, 1, 1, 0),
+	// vertical padding + font size from searchIcon
+	paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+	transition: theme.transitions.create('width'),
+	width: '100%',
+	[theme.breakpoints.up('sm')]: {
+	  width: '12ch',
+	  '&:focus': {
+		width: '20ch',
+	  },
+	},
+  },
+}),
+);
 
 export interface DialogTitleProps extends WithStyles<typeof styles> {
   id: string;
@@ -96,53 +139,62 @@ export default function Member() {
 
   return (
     <div>
-        <Card  variant = 'outlined' className='receipt-main-card'>
+        <div className='receipt-main-card'>
 			<CardContent  className='pb-0 pt-0'>	
 				<CardContent className='pt-0 pb-0' >
 					<Box display="flex" alignItems="center" flexWrap='wrap' justifyContent="space-between">
-						<h2 className='h1-title '>
+						<h4 className='h1-title '>
 							Members
-						</h2>
-						<Box display='flex' alignItems='center'>
-							<SearchIcon className='mr-10' />
-							<Button variant="contained" className={classes.button}> Add Member </Button>		
+						</h4>
+						<Box display='flex' alignItems='center'   justifyContent="space-between">
+							<div className={classes.search}>
+								<div className={classes.searchIcon}>
+								<SearchIcon />
+								</div>
+								<InputBase
+								placeholder="Search Member…"
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+								inputProps={{ 'aria-label': 'search' }}
+								/>
+							</div>
+							<Button variant="contained" className={classes.button}> Add </Button>		
 						</Box>
 					</Box>
 				</CardContent>	
 				<Grid container className={classes.root} spacing={2}>
-     			 	<Grid item xs={12} sm={6} md={6} lg={4}>
-						<CardContent >	
+     			 	<Grid item xs={6} sm={4} md={4} lg={3}>
+						
 					<Card>
 						<CardContent>
-							<Box display="flex" justifyContent="space-between">
-								<Box className="delete-dialogbox">	
-									<DeleteIcon  onClick={handleClickOpen} />								
+							<Box display="flex" justifyContent="space-between" className='mb-10'>
+								<Box className="delete-dialogbox dialogbox">	
+									<DeleteIcon  onClick={handleClickOpen} className='member_icon pr-0' />								
 									<Dialog className="delete-popup" onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
 										<DialogTitle id="customized-dialog-title" onClose={handleClose} >
-											Delete Member
+											<h5 className='m-0'>Delete Member</h5>
 										</DialogTitle>
-										<DialogContent >
-											<Typography gutterBottom style={{fontWeight: 500,margin:0 , fontSize: '13px' , textAlign: 'center'}}>
+										<DialogContent className='pt-0'  >
+											{/* <Typography gutterBottom style={{fontWeight: 500,margin:0 , fontSize: '13px' , textAlign: 'center'}}>
 												Are you sure you want to remove this member ?
-											</Typography>
-											<CardContent >	
-												<Card className='t-center b-radius'>
-													<CardContent>
+											</Typography> */}
+											<CardContent className='pt-0' >	
+													
 													<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'  className='member-photo mb-10' />	
-													<Typography style={{color: 'black' ,fontWeight: 500 , fontSize: '20px', paddingTop: '10px'}}> Yogesh Jadhav </Typography>
-													<Box className='label t-center' mb={2}> President </Box>
-													<Button variant = 'contained' className={classes.button}> 
-														Follow
-													</Button>
-													</CardContent>
-												</Card>		
-												
-											</CardContent>
+													<Typography className='t-center'> Yogesh Jadhav </Typography>
+													<Box className='label t-center'> President </Box>
+														{/* <Button variant = 'contained' className={classes.button}> 
+															Follow
+														</Button> */}
+													
+											</CardContent >
 											<Box className='t-center d-flex' justifyContent='space-evenly' alignItems='center'>
 												<Button variant = 'contained' className={classes.button}> 
-													Follow
+													Cancel
 												</Button> 
-												<Typography>
+												<Typography className='delete-message'>
 													<a href="/events"> Yes, I'am sure </a>
 												</Typography>
 											</Box>
@@ -150,45 +202,45 @@ export default function Member() {
 									</Dialog>
 								</Box>
 
-								<Box className="edit-dialogbox" >	
-									<EditIcon onClick={handleClickOpenEdit} />							
+								<Box className="edit-dialogbox dialogbox " >	
+									<EditIcon onClick={handleClickOpenEdit} className="member_icon"/>							
 									<Dialog className="delete-popup" onClose={handleClose} aria-labelledby="customized-dialog-title" open={openEdit}>
 										<DialogTitle id="customized-dialog-title" onClose={handleCloseEdit}>
-											Edit Member
+											<h5 className='m-0 h1'>Edit Member</h5>
 										</DialogTitle>
 									
-										<DialogContent >
-											<p className='member-photo t-center f-14 m-0'  >
+										<DialogContent className='pb-0 pt-0'>
+											{/* <p className='member-photo t-center f-14 m-0'  >
 												Are you sure you want to edit this member ?
-											</p>
-											<CardContent >			
+											</p> */}
+											<CardContent className='p-0'>			
 												<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg' className='member-photo' />	
-												<Box mt={2} mb={2}>
-													<span className='label' > Name </span>
-													<TextField id="outlined-basic" size='small' label="Enter the Name" variant="outlined" fullWidth />
-												</Box>
-												<Box mt={1} mb={2}>		
-													<span className='label'> Email Address </span>
-													<TextField id="outlined-basic" size='small' label="Enter the email" variant="outlined" fullWidth />
-												</Box>
-												<Box mt={1} mb={1}>
-													<span className='label' > Mobile Number </span>
-													<TextField id="outlined-basic" size='small' label="Enter the mob no" variant="outlined" fullWidth />
-												</Box>
+												
+												<div className='input-placeholder m-16'>
+													<span className='label' > Name</span>
+													<TextField id="outlined-basic"  size='small' placeholder="Enter the Name"  variant="outlined"  className='w-100'/>
+												</div>
+												<div className='input-placeholder m-16'>
+													<span className='label' > Email Address</span>
+													<TextField id="outlined-basic"  size='small' placeholder="Enter the email"  variant="outlined"  className='w-100'/>
+												</div>
+												<div className='input-placeholder m-16'>
+													<span className='label' > Mobile Number</span>
+													<TextField id="outlined-basic"  size='small' placeholder="Enter the mob no"  variant="outlined"  className='w-100'/>
+												</div>
+												<div className='t-right m-16'>
+													<Button  variant = 'contained' className={classes.button}> 
+														Save
+													</Button> 
+												</div>
 											</CardContent>
 										</DialogContent>
-
-										<DialogActions >
-											<Button variant = 'contained' className={classes.button}> 
-												Save
-											</Button> 
-										</DialogActions>
 									</Dialog>
 								</Box>
 							</Box>
 							<Box className='t-center' >
 								<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'  className='member-photo mb-10' />
-								<Typography variant='h6' > Abhijit Jachak </Typography>
+								<h4 className='mb-0 f-14'> Abhijit Jachak </h4>
 									<Box className='label' mb={2}> President </Box>
 								<Button variant = 'contained' className={classes.button}> 
 									Follow
@@ -196,100 +248,11 @@ export default function Member() {
 							</Box>
 						</CardContent>
 					</Card>				
-			</CardContent>
 					</Grid>
-					<Grid item xs={12} sm={6} md={6} lg={4}>
-						<CardContent >	
-							<Card>
-						<CardContent>
-							<Box display="flex" justifyContent="space-between">
-								<Box className="delete-dialogbox">	
-									<DeleteIcon  onClick={handleClickOpen} />								
-									<Dialog className="delete-popup" onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-										<DialogTitle id="customized-dialog-title" onClose={handleClose}>
-											Delete Member
-										</DialogTitle>
-										<DialogContent >
-											<Typography gutterBottom style={{fontWeight: 500 , fontSize: '13px' , textAlign: 'center'}}>
-												Are you sure you want to remove this member ?
-											</Typography>
-											<CardContent >	
-												<Card className='t-center b-radius'>
-													<CardContent>
-													<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'  className='member-photo mb-10' />	
-													<Typography style={{color: 'black' ,fontWeight: 500 , fontSize: '20px', paddingTop: '10px'}}> Akshay </Typography>
-													<Box className='label t-center' mb={2}> President </Box>
-													<Button variant = 'contained' className={classes.button}> 
-														Follow
-													</Button>
-													</CardContent>
-												</Card>		
-												
-											</CardContent>
-											<Box className='t-center d-flex' justifyContent='space-evenly' alignItems='center'>
-												<Button variant = 'contained' className={classes.button}> 
-													Follow
-												</Button> 
-												<Typography>
-													<a href="/events"> Yes, I'am sure </a>
-												</Typography>
-											</Box>
-										</DialogContent>
-									</Dialog>
-								</Box>
-
-								<Box className="edit-dialogbox" >	
-									<EditIcon onClick={handleClickOpenEdit} />							
-									<Dialog className="delete-popup" onClose={handleClose} aria-labelledby="customized-dialog-title" open={openEdit}>
-										<DialogTitle id="customized-dialog-title" onClose={handleCloseEdit}>
-											Edit Member
-										</DialogTitle>
-									
-										<DialogContent >
-											<p className='member-photo t-center f-14 m-0'  >
-												Are you sure you want to edit this member ?
-											</p>
-											<CardContent >			
-												<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg' className='member-photo' />	
-												<Box mt={2} mb={2}>
-													<span className='label' > Name </span>
-													<TextField id="outlined-basic" size='small' label="Enter the Name" variant="outlined" fullWidth />
-												</Box>
-												<Box mt={1} mb={2}>		
-													<span className='label'> Email Address </span>
-													<TextField id="outlined-basic" size='small' label="Enter the email" variant="outlined" fullWidth />
-												</Box>
-												<Box mt={1} mb={1}>
-													<span className='label' > Mobile Number </span>
-													<TextField id="outlined-basic" size='small' label="Enter the mob no" variant="outlined" fullWidth />
-												</Box>
-											</CardContent>
-										</DialogContent>
-
-										<DialogActions >
-											<Button variant = 'contained' className={classes.button}> 
-												Save
-											</Button> 
-										</DialogActions>
-									</Dialog>
-								</Box>
-							</Box>
-							<Box className='t-center' >
-								<Avatar alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'  className='member-photo mb-10' />
-								<Typography variant='h6'> Abhijit Jachak </Typography>
-								<Box className='label' mb={2}> President </Box>
-								<Button variant = 'contained' className={classes.button}> 
-									Follow
-								</Button> 
-							</Box>
-						</CardContent>
-					</Card>				
-						</CardContent>
-					</Grid>
-				</Grid>
+						</Grid>
 				</CardContent>
 				
-		</Card>
+		</div>
    	</div>
   );
 }
