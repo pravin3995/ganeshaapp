3b427177b659bd6263f53e2da8ad05375ef9e1cc
pravin <pravinjadhav771@gmail.com>
2020-04-13 20:42:04 +0530

    event slider

--- src/components/eventUpdated.tsx
+++ src/components/eventUpdated.tsx
@@ -13,45 +13,74 @@ import "slick-carousel/slick/slick-theme.css";
 import './../custom.scss'
 
 export default function eventUpdated() {
-    const settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 2.3,
-        slidesToScroll: 1,
-        // centerMode: true,
-        // centerPadding: '40px',
+    let settings = {
+        centerMode: true,
+        centerPadding: '200px',
+        focusOnSelect: false,
+        slidesToShow: 1,
+        speed:400,
+        
+        // afterChange: current => this.setState({ activeSlide: current }),
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2.3,
-                    
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1.3
-                }
+          {
+            breakpoint: 992,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '200px',
+              focusOnSelect: false,
+              slidesToShow: 1
             }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '100px',
+              focusOnSelect: false,
+              slidesToShow: 1
+            }
+          },
+          {
+            breakpoint: 580,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '100px',
+              focusOnSelect: false,
+              slidesToShow: 1
+            }
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              focusOnSelect: false,
+              slidesToShow: 1
+            }
+          }
 
         ]
     }
     return (
         <div className="main-wrapper">
             <h2> Upcoming Events</h2>
-            <Slider {...settings}>
+            <Slider {...settings} className='list'>
                 <div className="px-3">
-                    <Card>
+                    <Card className='slider-card'>
                         <CardContent >
                             <span className="label">Feb 27,2020</span>
                             <Typography variant='h6' component='h2' gutterBottom >
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -63,15 +92,17 @@ export default function eventUpdated() {
                     </Card>
                 </div>
                 <div className="px-3">
-                    <Card>
+                    <Card className='slider-card'>
                         <CardContent >
                             <span className="label">Feb 27,2020</span>
                             <Typography variant='h6' component='h2' gutterBottom >
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -83,15 +114,17 @@ export default function eventUpdated() {
                     </Card>
                 </div>
                 <div className="px-3">
-                    <Card>
+                    <Card className='slider-card'>
                         <CardContent >
                             <span className="label">Feb 27,2020</span>
                             <Typography variant='h6' component='h2' gutterBottom >
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -103,15 +136,17 @@ export default function eventUpdated() {
                     </Card>
                 </div>
                 <div className="px-3">
-                    <Card>
+                    <Card className='slider-card'>
                         <CardContent >
                             <span className="label">Feb 27,2020</span>
                             <Typography variant='h6' component='h2' gutterBottom >
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -123,15 +158,17 @@ export default function eventUpdated() {
                     </Card>
                 </div>
                 <div className="px-3">
-                    <Card>
+                    <Card className='slider-card'>
                         <CardContent >
                             <span className="label">Feb 27,2020</span>
                             <Typography variant='h6' component='h2' gutterBottom >
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                                 <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -159,8 +196,73 @@ export default function eventUpdated() {
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>							</Box>
+                            <Box className="mandal-premises">
+                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
+                                    <LocationOnIcon />
+                                    <span >Mandal Premises </span>
+                                </Link>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="px-3">
+                    <Card>
+                        <CardContent >
+                            <span className="label">Feb 27,2020</span>
+                            <Typography variant='h6' component='h2' gutterBottom >
+                                Ganesh Chaturthi
+							</Typography>
+                            <Box mb={2} className='ganesh-chathurti-description'>
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>							</Box>
+                            <Box className="mandal-premises">
+                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
+                                    <LocationOnIcon />
+                                    <span >Mandal Premises </span>
+                                </Link>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="px-3">
+                    <Card>
+                        <CardContent >
+                            <span className="label">Feb 27,2020</span>
+                            <Typography variant='h6' component='h2' gutterBottom >
+                                Ganesh Chaturthi
+							</Typography>
+                            <Box mb={2} className='ganesh-chathurti-description'>
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>							</Box>
+                            <Box className="mandal-premises">
+                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
+                                    <LocationOnIcon />
+                                    <span >Mandal Premises </span>
+                                </Link>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="px-3">
+                    <Card>
+                        <CardContent >
+                            <span className="label">Feb 27,2020</span>
+                            <Typography variant='h6' component='h2' gutterBottom >
+                                Ganesh Chaturthi
+							</Typography>
+                            <Box mb={2} className='ganesh-chathurti-description'>
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
@@ -179,68 +281,10 @@ export default function eventUpdated() {
                                 Ganesh Chaturthi
 							</Typography>
                             <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-							</Box>
-                            <Box className="mandal-premises">
-                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
-                                    <LocationOnIcon />
-                                    <span >Mandal Premises </span>
-                                </Link>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </div>
-                <div className="px-3">
-                    <Card>
-                        <CardContent >
-                            <span className="label">Feb 27,2020</span>
-                            <Typography variant='h6' component='h2' gutterBottom >
-                                Ganesh Chaturthi
-							</Typography>
-                            <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-							</Box>
-                            <Box className="mandal-premises">
-                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
-                                    <LocationOnIcon />
-                                    <span >Mandal Premises </span>
-                                </Link>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </div>
-                <div className="px-3">
-                    <Card>
-                        <CardContent >
-                            <span className="label">Feb 27,2020</span>
-                            <Typography variant='h6' component='h2' gutterBottom >
-                                Ganesh Chaturthi
-							</Typography>
-                            <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-							</Box>
-                            <Box className="mandal-premises">
-                                <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
-                                    <LocationOnIcon />
-                                    <span >Mandal Premises </span>
-                                </Link>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </div>
-                <div className="px-3">
-                    <Card>
-                        <CardContent >
-                            <span className="label">Feb 27,2020</span>
-                            <Typography variant='h6' component='h2' gutterBottom >
-                                Ganesh Chaturthi
-							</Typography>
-                            <Box mb={2} className='ganesh-chathurti-description'>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                               <p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                </p>
 							</Box>
                             <Box className="mandal-premises">
                                 <Link to="https://goo.gl/maps/r3Zxt2iM9zrurHdD8" target='_blank' >
