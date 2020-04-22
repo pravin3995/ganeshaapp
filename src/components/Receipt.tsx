686fb3e7e87d1b657dadd344ec192b465490135c
pravin <pravinjadhav771@gmail.com>
2020-04-22 14:50:53 +0530

    receipt slider

--- src/components/Receipt.tsx
+++ src/components/Receipt.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
@@ -12,9 +12,6 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from 'react-slick'
-
-
-
 const style ={
   AlignMent:{
     display:'flex',
@@ -40,24 +37,31 @@ const useStyles  =  makeStyles((theme: Theme)  =>
       padding: theme.spacing(2),
     },
     button: {
-      backgroundColor: '#df3088',
+      backgroundColor: '#DF3088',
       borderRadius: '15px',
       color: 'white',
       padding:'6px 16px'
     }
   }),
 );
-
 export default function ReceiptDetails() {
   const classes  =  useStyles()
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
+  const slider1 = useRef(null);
+  const slider2 = useRef(null);
+  //i dont seem to need this
+  useEffect(() => {
+    setNav1(slider1.current);
+    setNav2(slider2.current);
+  }, []);
   let settings = {
     // centerMode: true,
     // centerPadding: '200px',
     focusOnSelect: false,
     slidesToShow: 1,
     speed:400,
-    // fade: true,
-    
+    fade: true,
     // afterChange: current => this.setState({ activeSlide: current }),
     responsive: [
       {
@@ -100,26 +104,50 @@ export default function ReceiptDetails() {
       //     slidesToShow: 1
       //   }
       // }
-
     ]
 }
+const sliderone = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true
+};
+const slidertwo = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true
+};
   return (
-     <div className='receipt-details '>
-       <div className="year-slider t-center">
-          <h4 className='mb-0'>Select Year</h4>
-            <Slider {...settings} className='year-list'>
-                <div className='t-center'>
-                    <h5 className='year'>2020</h5>
-                </div>
-                <div className='t-center'>
-                    <h5 className='year'>2019</h5>
-                </div>
-                <div className='t-center'>
-                    <h5 className='year'>2018</h5>
-                </div>
-             </Slider>
-          
-        </div>
+    <div className='receipt-details '>
+    <div className="year-slider t-center">
+       <h4 className='mb-0'>Select Year</h4>
+    <Slider {...settings} className='year-list' asNavFor={nav2} ref={slider1}>
+    <div className='t-center'>
+          <h5 className='year'>2020</h5>
+      </div>
+      <div className='t-center'>
+          <h5 className='year'>2019</h5>
+      </div>
+      <div className='t-center'>
+          <h5 className='year'>2018</h5>
+      </div>
+    </Slider>
+     <Slider
+      asNavFor={nav1}
+      ref={slider2}
+      slidesToShow={1}
+      swipeToSlide={true}
+      focusOnSelect={true}
+      fade={true}
+      speed={400}
+      arrows={false}
+    >
+       <Box mb={2}mt={2}>
             <CardContent className='pt-0 receipt-size' >  
                 <Card elevation={0} className='p-relative'>
                   <div className="circle-shape circle-4"><span>Paid</span></div>
@@ -127,15 +155,12 @@ export default function ReceiptDetails() {
                       <FiberManualRecordIcon className='icon left-icon'/>
                       <FiberManualRecordIcon className='icon right-icon'/>
                     </div> */}
-                  
                   <CardContent className='card-wrap'>
                     <Box className=' payment-suc' pt={1} pb={3}>
-                    <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/>	
-
+                    <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/> 
                         <h2 className="successfully-text"> Lions Club Mitra Mandal</h2>
                         <p className='transaction-n'> Dattawadi , pune - 30</p>
                         <p className='transaction-n'> Reg. no : 553 </p>
-
                       </Box>
                       <Box className=' py-20'> 
                         <Grid container>
@@ -145,13 +170,11 @@ export default function ReceiptDetails() {
                               '> Receipt Number: </span>
                               <span className='f-bold'> #002 </span>
                             </Box>
-                            
                             <Box mt={2} mb={2} > 
                               <span className='label d-block
                               '> Full Name: </span>
                               <span className='f-bold'> Baldev Pardesi </span>
                             </Box>
-                            
                             <Box mt={2} mb={2} > 
                               <span className='label d-block
                               '> Received by: </span>
@@ -179,8 +202,142 @@ export default function ReceiptDetails() {
                   </Box>
                   </CardContent>
                 </Card>
-              
             </CardContent>
-			</div>
-  )
-}
+      </Box>
+      <Box mb={2}mt={2}>
+            <CardContent className='pt-0 receipt-size' >  
+                <Card elevation={0} className='p-relative'>
+                  <div className="circle-shape circle-4"><span>Paid</span></div>
+                  {/* <div className='circle'>
+                      <FiberManualRecordIcon className='icon left-icon'/>
+                      <FiberManualRecordIcon className='icon right-icon'/>
+                    </div> */}
+                  <CardContent className='card-wrap'>
+                    <Box className=' payment-suc' pt={1} pb={3}>
+                    <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/> 
+                        <h2 className="successfully-text"> Lions Club Mitra Mandal</h2>
+                        <p className='transaction-n'> Dattawadi , pune - 30</p>
+                        <p className='transaction-n'> Reg. no : 553 </p>
+                      </Box>
+                      <Box className=' py-20'> 
+                        <Grid container>
+                          <Grid item xs = {6}>
+                            <Box mt={2} mb={2}> 
+                              <span className='label d-block
+                              '> Receipt Number: </span>
+                              <span className='f-bold'> #002 </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Full Name: </span>
+                              <span className='f-bold'> Baldev Pardesi </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Received by: </span>
+                              <span className='f-bold'> Akshay </span>
+                            </Box>
+                          </Grid>
+                          <Grid item xs = {6}>
+                            <Box mt={2} mb={2}> 
+                              <span className='label d-block
+                              '> Amount: </span>
+                              <span className='f-bold receipt-amout'> 5000 /- </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Date: </span>
+                              <span className='f-bold'> 8 December 2020 </span>
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                      <Box pt={2}>
+                    <Typography variant="h5" className='f-bold t-center thank-you-title'>
+                      Thank You!
+                    </Typography>
+                  </Box>
+                  </CardContent>
+                </Card>
+            </CardContent>
+      </Box>
+      <Box mb={2}mt={2}>
+            <CardContent className='pt-0 receipt-size' >  
+                <Card elevation={0} className='p-relative'>
+                  <div className="circle-shape circle-4"><span>Paid</span></div>
+                  {/* <div className='circle'>
+                      <FiberManualRecordIcon className='icon left-icon'/>
+                      <FiberManualRecordIcon className='icon right-icon'/>
+                    </div> */}
+                  <CardContent className='card-wrap'>
+                    <Box className=' payment-suc' pt={1} pb={3}>
+                    <Avatar variant='square'  className={classes.large} alt = 'Jay Ganesh' src = 'https://i.pinimg.com/originals/6d/a6/03/6da6030949f48ddd94d4ce117da336d5.jpg'/> 
+                        <h2 className="successfully-text"> Lions Club Mitra Mandal</h2>
+                        <p className='transaction-n'> Dattawadi , pune - 30</p>
+                        <p className='transaction-n'> Reg. no : 553 </p>
+                      </Box>
+                      <Box className=' py-20'> 
+                        <Grid container>
+                          <Grid item xs = {6}>
+                            <Box mt={2} mb={2}> 
+                              <span className='label d-block
+                              '> Receipt Number: </span>
+                              <span className='f-bold'> #002 </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Full Name: </span>
+                              <span className='f-bold'> Baldev Pardesi </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Received by: </span>
+                              <span className='f-bold'> Akshay </span>
+                            </Box>
+                          </Grid>
+                          <Grid item xs = {6}>
+                            <Box mt={2} mb={2}> 
+                              <span className='label d-block
+                              '> Amount: </span>
+                              <span className='f-bold receipt-amout'> 5000 /- </span>
+                            </Box>
+                            <Box mt={2} mb={2} > 
+                              <span className='label d-block
+                              '> Date: </span>
+                              <span className='f-bold'> 8 December 2020 </span>
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                      <Box pt={2}>
+                    <Typography variant="h5" className='f-bold t-center thank-you-title'>
+                      Thank You!
+                    </Typography>
+                  </Box>
+                  </CardContent>
+                </Card>
+            </CardContent>
+      </Box>
+    </Slider>
+  </div>
+  </div>
+     )
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
